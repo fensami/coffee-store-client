4676f94d92aa293e70e9967d68d3325a0059f7dc
fensami <113832498+fensami@users.noreply.github.com>
2023-05-22 09:30:12 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useLoaderData } from 'react-router-dom'
+import './App.css'
+import CoffeeCard from './components/CoffeeCard'
+
+function App() {
+
+  const coffees = useLoaderData()
+
+
+  return (
+    <div className='m-20'>
+      <h1 className='text-6xl text-center text-purple-600'>Vite + React {coffees.length}</h1>
+
+      <div className='grid md:grid-cols-2 my-20 gap-4'>
+        {
+          coffees.map(coffee => <CoffeeCard
+            key={coffee._id}
+            coffee={coffee}
+          ></CoffeeCard>)
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
